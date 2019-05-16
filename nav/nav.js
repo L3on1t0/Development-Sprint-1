c0cacb982b21a31a027a0c99ac85d3958fffc046
fariafarjana <fariafarzana.03@gmail.com>
2019-05-16 18:48:11 +0930

    updated nav template
    Signed-off-by: fariafarjana <fariafarzana.03@gmail.com>

---
+++ nav/nav.js
@@ -0,0 +1,15 @@
+const navSlide = () => {
+  const menu = document.querySelector('.menu');
+  const topnav = document.querySelector('.nav-links');
+
+  menu.addEventListener('click', () =>{
+    topnav.classList.toggle('nav-active');
+
+    // if(topnav.style.display!='none'){
+    //   topnav.style.display='none';
+    // }else{
+    //   topnav.style.display='inherit';
+    // }
+  });
+}
+navSlide();
